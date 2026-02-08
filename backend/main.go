67b921a5bb2b4d09110a1fd5730f18c50bfc8ada
YDevBB <yasinnosi@gmail.com>
2026-02-08 16:43:57 +0100

    For automatic run of workflow

--- backend/main.go
+++ backend/main.go
@@ -212,7 +212,7 @@ func (app *App) createEntryHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Invalidera cachee
+	// Invalidera cache
 	if app.Redis != nil {
 		app.Redis.Del(app.Ctx, "entries:all")
 		// Incrementera statistik
